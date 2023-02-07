bfca4641d97ac38164dc10fca55844c2d124c393
strps <csrstrps@gmail.com>
2023-02-07 16:57:05 +0000

    Addig routes and models to the back end

--- src/api/models.py
+++ src/api/models.py
@@ -3,13 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
@@ -17,3 +18,291 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class Planet(db.Model):
+    __tablename__ = "planet"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False, unique=True)
+    diameter = db.Column(db.Integer)
+    rotation_period = db.Column(db.Float)
+    orbital_period = db.Column(db.Float)
+    gravity = db.Column(db.Float)
+    population = db.Column(db.Integer)
+    climate = db.Column(db.String(50))
+    terrain = db.Column(db.String(50))
+    surface_water = db.Column(db.Float)
+    created = db.Column(db.DateTime)
+    edited = db.Column(db.DateTime)
+    created_by_id = db.Column(
+    db.Integer, db.ForeignKey('user.id'), nullable=False)
+    created_by = db.relationship('User')
+
+    def __repr__(self):
+        return '<Planet %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "created_by": self.created_by.serialize()
+        }
+
+
+class Character(db.Model):
+    __tablename__ = 'character'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False, unique=True)
+    height = db.Column(db.Integer)
+    mass = db.Column(db.Integer)
+    hair_color = db.Column(db.String(50))
+    skin_color = db.Column(db.String(50))
+    eye_color = db.Column(db.String(50))
+    birth_year = db.Column(db.String(50))
+    gender = db.Column(db.String(50))
+    created = db.Column(db.DateTime)
+    edited = db.Column(db.DateTime)
+    # homeworld_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    # homeworld = db.relationship('Planet')
+    created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_by = db.relationship(User)
+
+    def __repr__(self):
+        return '<Character %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "created": self.created,
+            "edited": self.edited,
+            # "homeworld_id": self.homeworld_id,
+            # "homeworld": self.homeworld,
+            "created_by_id": self.created_by_id,
+            "created_by": self.created_by.serialize()
+        }
+
+
+
+class Films(db.Model):
+    __tablename__ = "films"
+    id = db.Column(db.Integer, primary_key=True)
+    # characters= db.Column()       
+    director = db.Column(db.String(120))
+    created = db.Column(db.DateTime)
+    edited = db.Column(db.DateTime)
+    episode_id = db.Column(db.Integer)
+    opening_crawl = db.Column(db.String(120))
+    # planets= db.Column()
+    producer = db.Column(db.String(120))
+    release_date = db.Column(db.Date)
+    # species= db.Column()
+    # starships= db.Column()
+    title = db.Column(db.String(120))
+    # url= db.Column()
+    # vehicles= db.Column()
+
+    def __repr__(self):
+        return '<Film %r>' % self.title
+
+    def serialize(self):
+        return {
+        "director":self.director, 
+        "created":self.created,
+        "edited":self.edited,
+        "episode_id":self.episode_id,
+        "opening_crawl":self.opening_crawl,
+        # planets
+        "producer":self.producer,
+        "release_date":self.release_date,
+        # species
+        # starships
+        "title":self.title,
+        }
+
+class Starships(db.Model):
+    __tablename__ = "starships"
+    id = db.Column(db.Integer, primary_key=True)
+    MGLT = db.Column(db.String(120))  
+    cargo_capacity = db.Column(db.Integer)  
+    consumables = db.Column(db.String(120)) 
+    cost_in_credits = db.Column(db.Integer)  
+    created = db.Column(db.DateTime)  
+    crew = db.Column(db.Integer) 
+    edited = db.Column(db.DateTime) 
+    hyperdrive_rating = db.Column(db.String())  
+    length = db.Column(db.Integer)  
+    manufacturer = db.Column(db.String(120))
+    max_atmosphering_speed = db.Column(db.Integer)
+    model = db.Column(db.String(120))
+    name = db.Column(db.String(120))
+    passengers = db.Column(db.Integer)
+    # films= db.Column()
+    # pilots= db.Column()
+    # ": "Deep Space Mobile Battlestation",
+    starship_class = db.Column(db.String(120))
+    # url= db.Column()
+
+    def __repr__():
+        return '<Starship %r>' % self.name
+
+    def serialize(self):
+        return {
+        "MGLT": self.MGLT,
+        "cargo_capacity": self.cargo_capacity,
+        "consumables": self.consumables,
+        "cost_in_credits": self.cost_in_credits,
+        "created": self.created,
+        "crew": self.crew,
+        "edited": self.edited,
+        "hyperdrive_rating": self.hyperdrive_rating,
+        "length": self.length,
+        "manufacturer": self.manufacturer,
+        "max_atmosphering_speed": self.max_atmosphering_speed,
+        "model": self.model,
+        "name": self.name,
+        "passengers": self.passengers,
+        # "films": self.films,
+        # "pilots": self.pilots,
+        "starship_class": self.starship_class
+        # "url": self.url
+        }
+
+
+
+class Vehicles(db.Model):
+    __tablename__ = 'vehicles'
+    id = db.Column(db.Integer, primary_key=True)
+    cargo_capacity = db.Column(db.Integer)
+    consumables = db.Column(db.String(120))
+    cost_in_credits = db.Column(db.Integer)
+    created = db.Column(db.DateTime)
+    crew = db.Column(db.Integer)
+    edited = db.Column(db.DateTime())
+    length = db.Column(db.Integer)
+    manufacturer = db.Column(db.String(120))
+    max_atmosphering_speed = db.Column(db.Integer)
+    model = db.Column(db.String(120))
+    name = db.Column(db.String(120))
+    passengers = db.Column(db.Integer)
+    # pilots=db.Column()
+    # films=db.Column()
+    # url=db.Column()
+    vehicle_class = db.Column(db.String(120))
+
+    def serialize(self):
+        return {
+        "cargo_capacity": self.cargo_capacity,
+        "consumables": self.consumables,
+        "cost_in_credits": self.cost_in_credits,
+        "created": self.created,
+        "crew": self.crew,
+        "edited": self.edited,
+        "length": self.length,
+        "manufacturer": self.manufacturer,
+        "max_atmosphering_speed": self.max_atmosphering_speed,
+        "model": self.model,
+        "name": self.name,
+        "passengers": self.passengers,
+        "vehicle_class": self.vehicle_class
+        }
+
+
+class Species(db.Model):
+    __tablename__ = "species"
+    id = db.Column(db.Integer, primary_key=True)
+    average_height = db.Column(db.Integer)
+    average_lifespan = db.Column(db.Integer)
+    classification = db.Column(db.String(120))
+    created = db.Column(db.DateTime)
+    designation = db.Column(db.String(120))
+    edited = db.Column(db.DateTime)
+    eye_colors = db.Column(db.String(120))
+    hair_colors = db.Column(db.String(120))  
+    #homeworld = db.Column(db.String(120))
+    language = db.Column(db.String(120))
+    name = db.Column(db.String(120))
+    #people = db.Column(db.String(120))
+    #films = db.Column(db.String(120))
+    skin_colors = db.Column(db.String(120))
+    #url = db.Column(db.String(120))
+
+    def serialize(self):
+        return {
+            "average_height": self.average_height,
+            "average_lifespan": self.average_lifespan,
+            "classification": self.classification,
+            "created": self.created.strftime("%Y-%m-%d %H:%M:%S"),
+            "designation": self.designation,
+            "edited": self.edited.strftime("%Y-%m-%d %H:%M:%S"),
+            "eye_colors": self.eye_colors,
+            "hair_colors": self.hair_colors,
+            "language": self.language,
+            "name": self.name,
+            "skin_colors": self.skin_colors,
+        }
+
+
+class Favorites_Planets(db.Model):
+    '''Favorites Planets'''
+    __tablename__ = 'favorites_planets'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User')
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    planet = db.relationship('Planet')
+
+
+class Favorites_Characters(db.Model):
+    '''Favorites Characters'''
+    __tablename__ = 'favorites_characters'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User')
+    planet_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    planet = db.relationship('Character')
+
+
+class Favorites_Vehicles(db.Model):
+    '''Favorites Vehicles'''
+    __tablename__ = 'favorites_vehicles'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User')
+    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicles.id'))
+    vehicle = db.relationship('Vehicles')
+
+
+class Favorites_Species(db.Model):
+    '''Favorites Species'''
+    __tablename__ = 'favorites_species'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User')
+    species_id = db.Column(db.Integer, db.ForeignKey('species.id'))
+    species = db.relationship('Species')
+
+class Favorites_Films(db.Model):
+    '''Favorites Films'''
+    __tablename__ = 'favorites_films'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User')
+    film_id = db.Column(db.Integer, db.ForeignKey('films.id'))
+    film = db.relationship('Films')
+
+class Favorites_Starships(db.Model):
+    '''Favorites Starships'''
+    __tablename__ = 'favorites_starships'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User')
+    starship_id = db.Column(db.Integer, db.ForeignKey('starships.id'))
+    starship = db.relationship('Starships')
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,10 +2,181 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Character, Planet, Species, Vehicles, Starships, Films
+
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
+
+
+
+################ Characters Routes ################
+
+@api.route('/characters')
+def get_characters():
+    characters = Character.query.all()
+    response_body = list(map(lambda c: c.serialize(), characters))
+    return jsonify(response_body)
+
+@api.route('/characters/<int:id>')
+def get_character(id):
+    character = Character.query.get(id).serialize()
+    if character is None:
+        return jsonify({"msg":"character not found"}), 404
+    return jsonify(character)
+
+
+################ Planets Routes ################
+
+@api.route('/planets')
+def get_planets():
+    limit = request.args.get("limit", 10 ,type=int)
+    offset = request.args.get("offset", 0 ,type=int)
+
+    planets = Planet.query.offset(offset).limit(limit).all()
+    response_body = list(map(lambda p: p.serialize(), planets))
+    return jsonify(response_body)
+
+@api.route('/planets/<int:id>')
+def get_planet(id):
+    planet = Planet.query.get(id).serialize()
+    if planet is None:
+        return jsonify({"msg":"planet not found"}), 404
+    return jsonify(planet), 200
+
+@api.route("/planets", methods=['POST'])
+def create_planet():
+    name = request.json.get('name')
+    gravity = request.json.get('gravity')
+    created_by_id = request.json.get("created_by_id")
+    new_planet = Planet(name = name, gravity = gravity, created_by_id = created_by_id)
+    db.session.add(new_planet)
+    db.session.commit()
+    return "ok" , 201
+
+@api.route("/planets/<int:id>", methods = ['PATCH'])
+def update_planet(id):
+    planet = Planet.query.get(id)
+    if planet is None:
+        return jsonify({"msg":"planet not found"}), 404
+    if request.json.get('name') is not None:
+        planet.name = request.json.get('name')
+    if request.json.get('gravity') is not None:
+        planet.gravity = request.json.get("gravity")
+    if request.json.get('grcreated_by_idavity') is not None:
+        planet.created_by_id = request.json.get("created_by_id")
+
+    db.session.add(planet)
+    db.session.commit()
+
+    return jsonify(planet.serialize()), 200
+
+@api.route('/planets/<int:id>', methods=['DELETE'])
+def delete_planet(id):
+    planet = Planet.query.get(id)
+    if planet is None:
+        return jsonify({"msg":"planet not found"}), 404
+    db.session.delete(planet)
+    db.session.commit()
+    return jsonify({"msg":"Planet deleted"})
+
+################ Films Routes ################
+
+@api.route('/films/<int:film_id>', methods=['GET'])
+def get_film(film_id):
+    film = Films.query.get(film_id)
+    if not film:
+        return jsonify({'msg': 'Film not found'}), 404
+    return jsonify(film.serialize())
+
+@api.route('/films')
+def get_films():
+    limit = request.args.get("limit", 10 ,type=int)
+    offset = request.args.get("offset", 0 ,type=int)
+
+    films = Films.query.offset(offset).limit(limit).all()
+    return jsonify([film.serialize() for film in films])
+
+################ Starships Routes ################
+
+@api.route('/starships/<int:id>', methods=['GET'])
+def get_starship_by_id(id):
+    starship = Starships.query.get(id)
+    if not starship:
+        return jsonify({'msg': 'Starship not found'}), 404
+    return jsonify(starship.serialize())
+
+@api.route('/starships')
+def get_starships():
+    limit = request.args.get("limit", 10 ,type=int)
+    offset = request.args.get("offset", 0 ,type=int)
+
+    starships = Starships.query.offset(offset).limit(limit).all()
+    return jsonify([starship.serialize() for starship in starships])
+
+################ Vehicles Routes ################
+
+@api.route('/vehicles/<int:id>', methods=['GET'])
+def get_vehicle(id):
+    vehicle = Vehicles.query.get(id)
+    if not vehicle:
+        return jsonify({'msg': 'Vehicle not found'}), 404
+    return jsonify(vehicle.serialize())
+
+@api.route('/vehicles')
+def get_vehicles():
+    limit = request.args.get("limit", 10 ,type=int)
+    offset = request.args.get("offset", 0 ,type=int)
+
+    vehicles = Vehicles.query.offset(offset).limit(limit).all()
+    return jsonify([vehicle.serialize() for vehicle in vehicles])
+
+
+
+################ Species Routes ################
+
+@api.route('/species/<int:id>', methods=['GET'])
+def get_specie(id):
+    species = Species.query.get(id)
+    if not species:
+        return jsonify({'msg': 'Species not found'}), 404
+    return jsonify(species.serialize())
+
+@api.route('/species')
+def get_species():
+    limit = request.args.get("limit", 10 ,type=int)
+    offset = request.args.get("offset", 0 ,type=int)
+
+    species = Species.query.offset(offset).limit(limit).all()
+    return jsonify([species.serialize() for species in species])
+
+################ Users Routes ################
+
+@api.route('/users')
+def get_users():
+    users = User.query.all()
+    response_body = list(map(lambda u: u.serialize(), users))
+    return jsonify(response_body)
+
+@api.route("/users/favorites")
+def get_favorites():
+    return 'Favorites', 200
+
+@api.route("/favorite/planet/<int:planet_id>", methods= ["POST"])
+def add_favorite_planet():
+    '''Add favorite planet'''
+
+@api.route("/favorite/character/<int:planet_id>", methods= ["POST"])
+def add_favorite_character():
+    '''Add Favorite Character'''
+
+@api.route("/favorite/planet/<int:planet_id>", methods= ["DELETE"])
+def delete_favorite_planet():
+    '''Delete Favorite Planet'''
+
+@api.route("/favorite/character/<int:planet_id>", methods= ["DELETE"])
+def delete_favorite_character():
+    '''Delete Favorite Character'''
 
 
 @api.route('/hello', methods=['POST', 'GET'])
